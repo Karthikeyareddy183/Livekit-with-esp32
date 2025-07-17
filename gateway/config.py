aebfa05c3c0f4e8687cd3bb042ff488ee826f158
karthikeyareddy183 <karthikeyareddy183@gmail.com>
2025-07-17 16:29:40 +0530

    Deleted unwanted folder 'folder-name'

--- gateway/config.py
+++ gateway/config.py
@@ -2,11 +2,11 @@
 
 # Your LiveKit SFU endpoint (with WSS scheme)
 # e.g., "wss://your-livekit-server.com"
-LIVEKIT_WS_URL = "your_livekit_server_url"
+LIVEKIT_WS_URL = "wss://esp32-zlnycimc.livekit.cloud"
 
 # LiveKit API credentials
-LK_API_KEY = "your_livekit_api_key"  # e.g., "your_api_key"
-LK_API_SECRET = "your_livekit_api_secret"  # e.g., "your_api_secret"
+LK_API_KEY = "APIYka3Qd9S33Pf"  # e.g., "your_api_key"
+LK_API_SECRET = "QzpbR0cyoGcW45PEH2KFfp6Jz9equk56n3UMo4A2Zm4"  # e.g., "your_api_secret"
 
 # Room & participant settings
 ROOM_NAME = "test-room"
